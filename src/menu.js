74b9f5121858f4c5ed712a63d03974dc3b2a772b
t-k- <clock126@126.com>
2015-11-30 18:57:49 -0500

    update

--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,5 @@
-function ApplyToAllTabsOpened() {
-  chrome.windows.getAll({'populate': true}, function(windows) {
-    for (var i = 0; i < windows.length; i++) {
-      var tabs = windows[i].tabs;
-      for (var j = 0; j < tabs.length; j++) {
-        chrome.tabs.executeScript(tabs[j].id, {file: "jquery.min.js"});
-        chrome.tabs.executeScript(tabs[j].id, {file: "jquery.jplayer.min.js"});
-        chrome.tabs.executeScript(tabs[j].id, {file: "content.js"});
-      }
-    }
-  });
-}
-
 function selectionOnClick(info, tab) {
   chrome.tabs.sendMessage(tab.id, {my_msg: info.selectionText});
 }
 
 var selection = chrome.contextMenus.create({"title": "Voice Instead","contexts":["selection"],"onclick":selectionOnClick});
-
-ApplyToAllTabsOpened();
